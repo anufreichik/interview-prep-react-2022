30b7bbd5366546f4838003ffa5f416e7dc6e808a
anufreichik <marta149@hotmail.com>
2022-08-25 11:35:03 -0700

    flip card animation

--- src/components/Utils/FlippingCard.jsx
+++ src/components/Utils/FlippingCard.jsx
@@ -9,8 +9,15 @@ const FlippingCard = ({contentFront,contentBack}) => {
     return (
         <div className='card-container' >
             <div className={`card ${isFlipped?'flipped':''}`} onMouseEnter={handleClick} onMouseLeave={handleClick}>
-                <div className='front'>{contentFront}</div>
-                <div className='back'>{contentBack}</div>
+                <div className='front'>
+                    <h1>{contentFront}</h1>
+                    <img className="card-image" src="https://78.media.tumblr.com/d98fb931adb117c70f0dbced9e947520/tumblr_pe582mbWip1tlgv32o1_1280.png"></img>
+                </div>
+                <div className='back'>
+                    <h1>{contentBack}</h1>
+                    <p>Some sample text to demonstrate how these cards will work, including how they truncate long sentences. This section displays the full-length blog post.</p>
+                    <p>Bloggity bloggity bloggity blog. This would be the full text of the abbreviated blog post.</p>
+                </div>
             </div>
 
         </div>
